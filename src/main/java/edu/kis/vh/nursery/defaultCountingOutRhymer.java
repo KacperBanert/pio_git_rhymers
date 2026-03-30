a025f26eccd82c319673858658f6add69b2a8fe9
dm255657 <255657@edu.p.lodz.pl>
2026-03-30 16:04:04 +0200

    Poprawka literowki

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+public class DefaultCountingOutRhymer {
 
     public static final int CAPACITY = 12;
     public static final int UNINITIALIZED_TOTAL = -1;
